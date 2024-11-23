58591356097f183fb556d91217da5c6a2ddbec88
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2024-11-23 22:31:22 +0530

    remodified files

--- Tara/settings/default.py
+++ Tara/settings/default.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Directory where static files will be collected
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
