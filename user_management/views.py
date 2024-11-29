951f9a008e487d4424c5805a04a70e5f3ecbbd16
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2024-11-29 09:21:26 +0530

    Testing The Build Session

--- user_management/views.py
+++ user_management/views.py
@@ -177,6 +177,7 @@ def user_registration(request):
             logger.warning("Registration failed: Validation errors.")
             logger.debug(f"Validation errors: {serializer.errors}")
             print("************************************")
+            print("########################")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except IntegrityError as e:
             logger.error(f"Integrity error during registration: {str(e)}")
