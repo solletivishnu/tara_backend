db2f16135af694c87d4c271a3bcfafafe6f35c81
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2024-12-23 11:11:05 +0530

    Remodifed some use case

--- invoicing/views.py
+++ invoicing/views.py
@@ -1169,7 +1169,7 @@ def update_invoice(request, invoice_id):
 @permission_classes([IsAuthenticated])
 def delete_invoice(request, invoice_id):
     """
-    Delete an existing invoice by ID. To be tested new
+    Delete an existing invoice by ID. To be tested
     """
     try:
         invoice = Invoice.objects.filter(id=invoice_id).first()
