ff92272f1cb66bc2fbeefbca89a3f84a64ff5dff
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2024-12-26 12:51:38 +0530

    Remodifed goods and service data get api

--- invoicing/views.py
+++ invoicing/views.py
@@ -13,6 +13,7 @@ from django.http import QueryDict
 import logging
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.parsers import MultiPartParser, FormParser
+from django.http.response import JsonResponse,HttpResponse
 
 
 # Create loggers for general and error logs
@@ -889,29 +890,28 @@ def create_goods_service(request):
 )
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def retrieve_goods_service(request, id):
+def retrieve_goods_service(request, pk):
     """
     Retrieve a goods or service entry by ID.
     """
     try:
-        # Get all invoicing profiles associated with the user's business
-        invoicing_profiles = InvoicingProfile.objects.get(id=id)
-
-        if not invoicing_profiles.exists():
-            logger.warning(f"User {request.user.id} tried to access invoicing profiles, but none exist.")
-            return Response({"message": "No invoicing profiles found."}, status=status.HTTP_404_NOT_FOUND)
+        # Retrieve a single invoicing profile by ID
+        invoicing_profile = InvoicingProfile.objects.get(id=pk)
 
         # Serialize the data
-        serializer = InvoicingProfileGoodsAndServicesSerializer(invoicing_profiles, many=True)
+        serializer = InvoicingProfileGoodsAndServicesSerializer(invoicing_profile)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    except InvoicingProfile.DoesNotExist:
+        logger.warning(f"User {request.user.id} tried to access an invoicing profile with ID {pk}, but it does not exist.")
+        return Response({"message": "Invoicing profile not found."}, status=status.HTTP_404_NOT_FOUND)
+
     except Exception as e:
-        logger.error(f"Unexpected error in get_customer_profiles: {e}")
+        logger.error(f"Unexpected error in retrieve_goods_service: {e}")
         return Response(
             {"error": f"An unexpected error occurred: {e}"},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
-
 
 @swagger_auto_schema(
     method='put',
@@ -1191,3 +1191,213 @@ def delete_invoice(request, invoice_id):
             {"error": f"An unexpected error occurred: {e}"},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
+
+# def generate_document(self, generator, template_name):
+#     try:
+#         pdf = generator.generate_document(template_name)
+#         eventSerializer = Invoicing_profileSerializers(data=self.data)
+#         if eventSerializer.is_valid():
+#             eventSerializer.save()
+#         else:
+#             return Response(eventSerializer.errors, status=400)
+#
+#         response = HttpResponse(pdf, content_type='application/pdf')
+#         return response
+#     except Exception as e:
+#         log.error(f'Exception in generating document: {str(e)}')
+#         return Response({'error': str(e)}, status=500)
+#
+#
+# def formatStringDate(date):
+#     if isinstance(date, datetime):
+#         # If it's already a datetime object, format it directly
+#         return date.strftime('%d-%b-%Y')
+#     elif isinstance(date, str) and date:
+#         # If it's a string, parse it and then format
+#         try:
+#             parsed_date = datetime.strptime(date, '%d-%m-%Y')
+#             return parsed_date.strftime('%d-%b-%Y')
+#         except ValueError:
+#             return ''  # Handle invalid date formats as needed
+#     return ''  # Handle cases where date is None or an empty string
+#
+#
+# class DocumentGenerator:
+#     def __init__(self, request, invoicing_profile, contexts):
+#         self.request = request
+#         self.invoicing_profile = invoicing_profile
+#         self.contexts = contexts
+#
+#     def generate_document(self, template_name):
+#         try:
+#             # Render the HTML template with the context data
+#             html_content = render_to_string(template_name, {'contexts': self.contexts})
+#             html = HTML(string=html_content)
+#             pdf = html.write_pdf()
+#
+#             # Return the generated PDF as an HTTP response
+#             return HttpResponse(pdf, content_type='application/pdf')
+#         except Exception as e:
+#             log.error(f"Exception in generate_document: {str(e)}")
+#             raise
+#
+#
+# def split_address(address):
+#     half = len(address) // 2
+#     space_index = address.rfind(' ', 0, half)
+#
+#     if space_index != -1:
+#         first_half = address[:space_index]
+#         second_half = address[space_index + 1:]
+#     else:
+#         first_half = address[:half]
+#         second_half = address[half:]
+#
+#     return first_half + '<br/>' + second_half
+#
+#
+# @api_view(["GET"])
+# def createDocument(request):
+#     try:
+#         business_id = int(request.GET.get('business_id'))
+#         index = int(request.GET.get('index'))
+#         invoicing_profile = Invoicing_profile.objects.get(business_id_id=business_id)
+#
+#         signature_base64 = ''
+#         if invoicing_profile.signature:
+#             with open(invoicing_profile.signature.path, "rb") as image_file:
+#                 signature_base64 = base64.b64encode(image_file.read()).decode('utf-8')
+#
+#         if not invoicing_profile:
+#             return Response({'error': 'Invoicing profile not found'}, status=404)
+#
+#         # Assuming invoices is a list of invoice dictionaries
+#         invoices = invoicing_profile.invoices  # Replace with the correct field name if needed
+#         contexts = []
+#
+#         # Select the invoice based on the provided index
+#         selected_invoice = invoices[index] if len(invoices) > index else None
+#         item_details = selected_invoice.get('item_details', [])
+#         items = []
+#         for item in item_details:
+#             items.append({
+#                 'sno': item.get('sno', ''),
+#                 'item': item.get('item', ''),
+#                 'quantity': item.get('quantity', ''),
+#                 'unit_price': item.get('unitPrice', ''),
+#                 'hsn_sac': item.get('hsn_sac', ''),
+#                 'discount': item.get('discount', ''),
+#                 'amount': item.get('amount', ''),
+#                 'cgst': item.get('cgst', ''),
+#                 'sgst': item.get('sgst', ''),
+#                 'igst': item.get('igst', '')
+#             })
+#
+#         cgst = round(float(selected_invoice.get('cgst_amt', '0')), 2)
+#         sgst = round(float(selected_invoice.get('sgst_amt', '0')), 2)
+#
+#         subtotal = round(float(selected_invoice.get('subtotal', '0')), 2)
+#         cgst_amt = round(float(selected_invoice.get('cgst_amt', '0')), 2)
+#         sgst_amt = round(float(selected_invoice.get('sgst_amt', '0')), 2)
+#
+#         total = subtotal + cgst_amt + sgst_amt
+#         total_str = f"{total:.2f}"
+#         total_in_words = num2words(total)
+#         total_in_words = total_in_words.capitalize()
+#         half_length = len(total_in_words) // 2
+#         space_index = total_in_words.rfind(' ', 0, half_length)
+#         if space_index != -1:
+#             total_in_words = total_in_words[:space_index] + '<br/>' + total_in_words[space_index + 1:]
+#         else:
+#             total_in_words = total_in_words[:half_length] + '<br/>' + total_in_words[half_length:]
+#         invoice_date = selected_invoice.get('invoice_date', '')
+#         terms = int(selected_invoice.get('terms', 0) or 0) if selected_invoice else 0
+#         due_date = invoice_date + timedelta(days=terms)
+#         due_date_str = due_date.strftime('%d-%m-%Y')
+#
+#         if len(invoicing_profile.business_name) > 26:
+#             business_name = split_address(invoicing_profile.business_name)
+#             adjust_layout = True
+#         else:
+#             business_name = invoicing_profile.business_name
+#             adjust_layout = False
+#
+#         context = {
+#             'company_name': invoicing_profile.business_name.upper(),
+#             'business_type': invoicing_profile.business_type,
+#             'address': invoicing_profile.address,
+#             'state': invoicing_profile.state,
+#             'country': invoicing_profile.country,
+#             'pincode': invoicing_profile.pincode,
+#             'registration_number': invoicing_profile.registration_number,
+#             'gst_registered': invoicing_profile.gst_registered,
+#             'gstin': invoicing_profile.gstin,
+#             'email': invoicing_profile.email,
+#             'mobile': invoicing_profile.mobile,
+#             'pan': invoicing_profile.pan,
+#             'bank_name': invoicing_profile.bank_name,
+#             'account_number': invoicing_profile.account_number,
+#             'ifsc_code': invoicing_profile.ifsc_code,
+#             'signature': invoicing_profile.signature,
+#             'invoice_format': invoicing_profile.invoice_format,
+#             'signature': signature_base64,
+#
+#             # Invoice data fields (only for the selected invoice)
+#             'customer_name': selected_invoice.get('customer_name', '') if selected_invoice else '',
+#             'terms': selected_invoice.get('terms', '') if selected_invoice else '',
+#             'due_date': due_date_str,
+#             'financial_year': selected_invoice.get('financialYear', '') if selected_invoice else '',
+#             'invoice_number': selected_invoice.get('invoice_number', '') if selected_invoice else '',
+#             'invoice_date': formatStringDate(selected_invoice.get('invoice_date', '')) if selected_invoice else '',
+#             'place_of_supply': selected_invoice.get('placeofsupply', '') if selected_invoice else '',
+#
+#             # Bill To address fields
+#             'bill_to_address': selected_invoice.get('billto', {}).get('address', '') if selected_invoice else '',
+#             'bill_to_state': selected_invoice.get('billto', {}).get('state', '') if selected_invoice else '',
+#             'bill_to_country': selected_invoice.get('billto', {}).get('country', '') if selected_invoice else '',
+#             'bill_to_pincode': selected_invoice.get('billto', {}).get('pincode', '') if selected_invoice else '',
+#
+#             # Ship To address fields
+#             'ship_to_address': selected_invoice.get('shipto', {}).get('address', '') if selected_invoice else '',
+#             'ship_to_state': selected_invoice.get('shipto', {}).get('state', '') if selected_invoice else '',
+#             'ship_to_country': selected_invoice.get('shipto', {}).get('country', '') if selected_invoice else '',
+#             'ship_to_pincode': selected_invoice.get('shipto', {}).get('pincode', '') if selected_invoice else '',
+#
+#             # Item Details
+#             'item_details': items,
+#             'total': selected_invoice.get('total', 0) if selected_invoice else 0,
+#             'subtotal': f"{round(float(selected_invoice.get('subtotal', '0')), 2):.2f}",
+#             'cgst_amt': f"{round(float(selected_invoice.get('cgst_amt', '0')), 2):.2f}",
+#             'sgst_amt': f"{round(float(selected_invoice.get('sgst_amt', '0')), 2):.2f}",
+#             'total': total_str,
+#             'total_in_words': total_in_words,
+#             'cgst': cgst,
+#             'sgst': sgst,
+#             'igst_amt': selected_invoice.get('igst_amt', 0) if selected_invoice else 0,
+#             'payment_status': selected_invoice.get('payment_status', '') if selected_invoice else '',
+#             'terms_and_conditions': selected_invoice.get('termsAndConditions', '') if selected_invoice else '',
+#             'note': selected_invoice.get('note', '') if selected_invoice else '',
+#             'account_name': business_name,
+#             'adjust_layout': adjust_layout
+#         }
+#
+#         contexts.append(context)
+#         log.info("----------contexts", contexts)
+#
+#         # Assuming you have a DocumentGenerator class that generates the PDF
+#         document_generator = DocumentGenerator(request, invoicing_profile, contexts)
+#
+#         # Assuming `template_name` is the path to your HTML template
+#         template_name = "ETR.html"
+#
+#         # Generate the PDF document
+#         pdf_response = document_generator.generate_document(template_name)
+#
+#         # Return the PDF response
+#         return pdf_response
+#
+#     except Invoicing_profile.DoesNotExist:
+#         return Response({'error': 'Invoicing profile not found'}, status=404)
+#     except Exception as e:
+#         log.error('Exception in creating document: ' + str(e))
+#         return Response({'error': str(e)}, status=500)
