00991d5e2ab95b3878780d21e73ced8f74c1753f
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2025-04-28 16:27:53 +0530

    add permission classes to razorpay webhook for enhanced security

--- usermanagement/payment_webhooks.py
+++ usermanagement/payment_webhooks.py
@@ -9,12 +9,14 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from .models import PaymentInfo
 
 
 @csrf_exempt
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def razorpay_webhook(request):
     """Handle Razorpay Webhook Safely."""
     try:
