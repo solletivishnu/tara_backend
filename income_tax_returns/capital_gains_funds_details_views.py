9781595cf9e7e944047486d9bdcfdf99dd7a490f
tara-intern1 <dev1@tarafirst.com>
2025-06-03 12:24:50 +0530

    feat: update JSON handling for investment types and reinvestment details in capital gains views

--- income_tax_returns/capital_gains_funds_details_views.py
+++ income_tax_returns/capital_gains_funds_details_views.py
@@ -11,13 +11,13 @@ import json
 @parser_classes([MultiPartParser, FormParser])
 def upsert_equity_mutual_fund_with_files(request):
     try:
-        request_data = request.data
+        request_data = request.data.copy()
 
         # Coerce investment_types if sent as a string
         investment_types = request_data.get('equity_mutual_fund_type')
         if isinstance(investment_types, str):
             try:
-                request_data['equity_mutual_fund_type'] = json.loads(investment_types)
+                request_data['equity_mutual_fund_type'] = json.dumps(json.loads(investment_types))
             except json.JSONDecodeError:
                 return Response({"investment_types": "Invalid JSON format"}, status=400)
         service_request = request.data.get('service_request')
