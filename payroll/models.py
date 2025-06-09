78172f89e79c6f27a4bb40ecec687de8c57498af
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2025-06-09 12:26:52 +0530

    feat: add bulk employee upload functionality with Excel and CSV support

--- payroll/models.py
+++ payroll/models.py
@@ -117,7 +117,7 @@ class Departments(models.Model):
         ]
 
     def __str__(self):
-        return f"{self.dept_name} ({self.dept_code})"
+        return f"{self.dept_name}"
 
 
 class Designation(models.Model):
@@ -602,7 +602,8 @@ class EmployeePersonalDetails(BaseModel):
 
 
 class EmployeeBankDetails(BaseModel):
-    employee = models.OneToOneField('EmployeeManagement', on_delete=models.CASCADE, related_name='employee_bank_details')
+    employee = models.OneToOneField('EmployeeManagement', on_delete=models.CASCADE,
+                                    related_name='employee_bank_details')
     account_holder_name = models.CharField(max_length=150, null=False, blank=False)
     bank_name = models.CharField(max_length=150, null=False, blank=False)
     account_number = models.CharField(max_length=20, unique=True, null=False, blank=False)
