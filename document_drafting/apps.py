2c7e55eac2d3379bf4a3e948899dac6e1343887e
SKY-Saikiranyadav <saikiranyadav@financebox.in>
2025-07-23 13:44:31 +0530

    code push to test new

--- document_drafting/apps.py
+++ document_drafting/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+import sys
 
 class DocumentDraftingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'document_drafting'
+
+    def ready(self):
+        if 'gunicorn' in sys.argv or 'celery' in sys.argv:
+            try:
+                from Tara.scheduler_scripts.schedule_tasks import sync_schedules
+                sync_schedules()
+                print("✅ Celery schedule synced on startup.")
+            except Exception as e:
+                print(f"⚠️ Failed to sync schedule: {e}")
